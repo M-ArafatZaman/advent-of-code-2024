237b0726ecbaa0681a276457fed8e1b266ce7db7
M-ArafatZaman <mdarafatzaman1@gmail.com>
2024-12-01 11:48:06 -0500

    Clean uup

--- 1/a.py
+++ 1/a.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-with open("data.txt") as f:
-    data = [[int(j.strip()) for j in i.split("  ")] for i in f.read().splitlines()]
+with open("sample.txt") as f:
+    data = [list(map(int, i.split("   "))) for i in f.read().splitlines()]
     arr1, arr2 = np.array(data).T
+    arr1.sort(), arr2.sort()
     print(sum( [abs(i-j) for (i, j) in zip(arr1, arr2)] ))
-
