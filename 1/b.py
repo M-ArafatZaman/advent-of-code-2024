c7968ddabeb0f34076c67ca98943067b2d38b217
M-ArafatZaman <mdarafatzaman1@gmail.com>
2024-12-01 11:51:25 -0500

    Refactor

--- 1/b.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from collections import Counter
-
-with open("data.txt") as f:
-    data = [list(map(int, i.split("   "))) for i in f.read().splitlines()]
-    arr1, arr2 = np.array(data).T
-    c_arr2 = Counter(arr2)
-    print(sum( [ i * c_arr2.get(i, 0) for i in arr1 ] ))
--- 1/sol.py
+++ 1/sol.py
@@ -1,7 +1,11 @@
 import numpy as np
+from collections import Counter
 
 with open("sample.txt") as f:
     data = [list(map(int, i.split("   "))) for i in f.read().splitlines()]
     arr1, arr2 = np.array(data).T
     arr1.sort(), arr2.sort()
+    c_arr2 = Counter(arr2)
     print(sum( [abs(i-j) for (i, j) in zip(arr1, arr2)] ))
+    print(sum( [ i * c_arr2.get(i, 0) for i in arr1 ] ))
+    
