f11817d8843ea45ef784fc5d6f5b8ab7db6896f6
M-ArafatZaman <mdarafatzaman1@gmail.com>
2024-12-02 00:19:10 -0500

    Day 2 part 2

--- 2/sol.py
+++ 2/sol.py
@@ -1,5 +1,5 @@
 import numpy as np
-from itertools import accumulate
+import heapq
 
 def isInc(arr):
     for i in range(1, len(arr)):
@@ -13,15 +13,28 @@ def isDec(arr):
             return False
     return True
 
+def is_safe(i):
+    inc = isInc(i)
+    dec = isDec(i)
+
+    return inc ^ dec and max(np.diff(i)) <= 3 and min(np.diff(i)) >= -3
+
 with open('data.txt') as f:
     data = [ list(map(int, i.split(" "))) for i in f.read().splitlines()]
     safe = 0
+    safe_2 = 0
     for i in data:
-        inc = isInc(i)
-        dec = isDec(i)
-
-        if inc ^ dec and max(np.diff(i)) <= 3 and min(np.diff(i)) >= -3:
+        # Part 1
+        if is_safe(i):
             safe += 1
+            # Part 2
+            safe_2 += 1
+        else:
+            for j in range(len(i)):
+                if is_safe(i[:j] + i[j+1:]):
+                    safe_2 += 1
+                    break
 
     print(safe)
+    print(safe_2)
             
