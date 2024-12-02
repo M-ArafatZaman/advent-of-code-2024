1b4e0ed53c303511225d446257c5f51c2de809f4
M-ArafatZaman <mdarafatzaman1@gmail.com>
2024-12-02 00:22:18 -0500

    clean up

--- 2/sol.py
+++ 2/sol.py
@@ -1,5 +1,4 @@
 import numpy as np
-import heapq
 
 def isInc(arr):
     for i in range(1, len(arr)):
@@ -13,25 +12,22 @@ def isDec(arr):
             return False
     return True
 
-def is_safe(i):
-    inc = isInc(i)
-    dec = isDec(i)
-
-    return inc ^ dec and max(np.diff(i)) <= 3 and min(np.diff(i)) >= -3
+def is_safe(arr):
+    return isInc(arr) ^ isDec(arr) and max(np.diff(arr)) <= 3 and min(np.diff(arr)) >= -3
 
 with open('data.txt') as f:
     data = [ list(map(int, i.split(" "))) for i in f.read().splitlines()]
     safe = 0
     safe_2 = 0
-    for i in data:
-        # Part 1
-        if is_safe(i):
+    for arr in data:
+        if is_safe(arr):
+            # Part 1
             safe += 1
             # Part 2
             safe_2 += 1
         else:
-            for j in range(len(i)):
-                if is_safe(i[:j] + i[j+1:]):
+            for i in range(len(arr)):
+                if is_safe(arr[:i] + arr[i+1:]):
                     safe_2 += 1
                     break
 
