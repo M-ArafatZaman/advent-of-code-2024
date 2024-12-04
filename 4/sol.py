d349b6826f8bcbc46d2f07118cbbe67bbe16130e
M-ArafatZaman <mdarafatzaman1@gmail.com>
2024-12-04 01:09:42 -0500

    Part 2

--- 4/sol.py
+++ 4/sol.py
@@ -1,11 +1,12 @@
 import re
 import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
 
 # Use: python3 sol.py < data.txt
 with open(0) as f:
     data = [list(x.strip()) for x in f.readlines()]
-    print(np.array(data))
 
+    # Part 1
     count = 0
     # Search horizontally
     regex = r'XMAS'
@@ -29,4 +30,23 @@ with open(0) as f:
     for line in np.transpose(data):
         count += len(re.findall(regex, ''.join(line)))
         count += len(re.findall(regex, ''.join(line[::-1])))
+
+    # Part 2
+    count2 = 0
+    # Search chunks using sliding window
+    chunks = sliding_window_view(np.array(data), (3, 3))
+    for y in range(chunks.shape[0]):
+        for x in range(chunks.shape[1]):
+            chunk = chunks[y, x]
+            for i in range(len(chunk)):
+                # Diagonal that goes from top left to bottom right
+                diag = np.diag(chunk, k=i)
+                # Diagonal that goes from bottom left to top right
+                diag2 = np.diag(np.flipud(chunk), k=-i)
+                count += len(re.findall(regex, ''.join(diag)))
+                if (''.join(diag) == 'MAS' or ''.join(diag) == 'SAM') \
+                    and (''.join(diag2) == 'MAS' or ''.join(diag2) == 'SAM'):
+                    count2 += 1
+
     print(count)
+    print(count2)
