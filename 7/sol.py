89f37c75be35daa413fa8f03b2d2e87c7bf3cb29
M-ArafatZaman <mdarafatzaman1@gmail.com>
2024-12-07 00:37:01 -0500

    part 2

--- 7/sol.py
+++ 7/sol.py
@@ -1,46 +1,50 @@
 import sys
 from functools import cache
+import tqdm
 
 def get_input():
     data = open(sys.argv[1]).read().splitlines()
     return [(int(entry.split(":")[0]), list(map(int, entry.split(":")[1].strip().split(" ")))) for entry in data]
 
-def permutate(n):
+@cache
+def permutate(n, N = ("+", "*")):
     if n == 1:
-        return [["+"], ["*"]]
+        return [[x] for x in N]
     else:
-        prev = permutate(n-1)
-        return [x + ["+"] for x in prev] + [x + ["*"] for x in prev]
+        prev = permutate(n-1, N)
+        result = []
+        for x in prev:
+            for y in N:
+                result.append(x + [y])
+        return result
 
 def evaluate(vals, ops):
     total = vals[0]
     for op in range(len(ops)):
         if ops[op] == "+":
             total += vals[op+1]
-        else:
+        elif ops[op] == "*":
             total *= vals[op+1]
+        elif ops[op] == "||":
+            total = int(str(total) + str(vals[op+1]))
     return total
 
-def part1(data):
+def calibrate(data, operators = ("+", "*")):
     calibration = 0
-    for entry in data:
+    for entry in tqdm.tqdm(data):
         total, vals = entry
-        possibilities = permutate(len(vals)-1)
+        possibilities = permutate(len(vals)-1, operators)
         for possibility in possibilities:
             ops = list(possibility)
             if evaluate(vals, ops) == total:
                 calibration += total
                 break
     return calibration
-        
-
-def part2(data):
-    pass
 
 def main():
     data = get_input()
-    print(part1(data))
-    print(part2(data))
+    print(calibrate(data))
+    print(calibrate(data, ("+", "*", "||")))
 
 
 if __name__ == '__main__':
