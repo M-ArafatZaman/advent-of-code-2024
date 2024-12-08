bec10b13b9232c7bbae437f6d45a2e18b07be959
M-ArafatZaman <mdarafatzaman1@gmail.com>
2024-12-08 00:40:08 -0500

    Cleanup

--- 8/sol.py
+++ 8/sol.py
@@ -6,17 +6,15 @@ def get_input():
     return data
 
 def get_freqs(grid):
-    antenna_loc = set()
-    freq = defaultdict(list)
+    freqs = defaultdict(list)
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] != ".":
-                antenna_loc.add(complex(i, j))
-                freq[grid[i][j]].append(complex(i, j))
-    return antenna_loc, freq
+                freqs[grid[i][j]].append(complex(i, j))
+    return freqs
 
-def part1(antenna_loc, freqs, grid):
-    antinode = set()
+def solve(freqs, grid):
+    part1 = set()
     part2 = set()
     for freq in freqs:
         for i in range(len(freqs[freq])):
@@ -24,21 +22,23 @@ def part1(antenna_loc, freqs, grid):
                 if i == j: continue
                 freq_i = freqs[freq][i]
                 freq_j = freqs[freq][j]
-                antinode.add( freq_i - (freq_j - freq_i) )
-                antinode.add( freq_j - (freq_i - freq_j) )
+                # Part 1
+                part1.add( freq_i - (freq_j - freq_i) )
+                part1.add( freq_j - (freq_i - freq_j) )
+                # Part 2
                 for k in range(len(grid)):
                     part2.add( freq_i - k*(freq_j - freq_i) )
                     part2.add( freq_j - k*(freq_i - freq_j) )
     
     filter_func = lambda x: x.real >= 0 and x.real < len(grid) and x.imag >= 0 and x.imag < len(grid[0])
-    a = {i for i in antinode if filter_func(i)}
+    a = {i for i in part1 if filter_func(i)}
     b = {i for i in part2 if filter_func(i)}
-    return len(b)
+    return len(a), len(b)
 
 def main():
     grid = get_input()
-    antenna_loc, freqs = get_freqs(grid)
-    print(part1(antenna_loc, freqs, grid))
+    freqs = get_freqs(grid)
+    print(solve(freqs, grid))
 
 if __name__ == '__main__':
     main()
