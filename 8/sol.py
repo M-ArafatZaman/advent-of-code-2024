e5b853d7bacdd03fbcabcfc7de290fe7df9a8bba
M-ArafatZaman <mdarafatzaman1@gmail.com>
2024-12-08 00:35:44 -0500

    Part 2

--- 8/sol.py
+++ 8/sol.py
@@ -17,6 +17,7 @@ def get_freqs(grid):
 
 def part1(antenna_loc, freqs, grid):
     antinode = set()
+    part2 = set()
     for freq in freqs:
         for i in range(len(freqs[freq])):
             for j in range(len(freqs[freq])):
@@ -25,9 +26,14 @@ def part1(antenna_loc, freqs, grid):
                 freq_j = freqs[freq][j]
                 antinode.add( freq_i - (freq_j - freq_i) )
                 antinode.add( freq_j - (freq_i - freq_j) )
+                for k in range(len(grid)):
+                    part2.add( freq_i - k*(freq_j - freq_i) )
+                    part2.add( freq_j - k*(freq_i - freq_j) )
     
-    a = {node for node in antinode if node.real >= 0 and node.real < len(grid) and node.imag >= 0 and node.imag < len(grid[0]) }
-    return len(a)
+    filter_func = lambda x: x.real >= 0 and x.real < len(grid) and x.imag >= 0 and x.imag < len(grid[0])
+    a = {i for i in antinode if filter_func(i)}
+    b = {i for i in part2 if filter_func(i)}
+    return len(b)
 
 def main():
     grid = get_input()
