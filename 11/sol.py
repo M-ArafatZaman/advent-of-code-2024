91fcaabda6156a95dd148f0662211809f3c9a2b6
M-ArafatZaman <mdarafatzaman1@gmail.com>
2024-12-11 00:39:28 -0500

    Part 1 optimized

--- 11/sol.py
+++ 11/sol.py
@@ -1,26 +1,22 @@
-import tqdm
+from functools import cache
 
-def part1(data):
-    arr = list(data)
-    BLINK = 25
-    for _ in tqdm.tqdm(range(BLINK)):
-        i = 0
-        while i < len(arr):
-            if arr[i] == 0:
-                arr[i] = 1
-            elif len(str(arr[i])) % 2 == 0:
-                num = str(arr[i])
-                arr[i] = int(str(num)[:len(num)//2])
-                arr.insert(i+1, int(str(num)[len(num)//2:]))
-                i += 1
-            else:
-                arr[i] = arr[i] * 2024
-            i += 1
-    return len(arr)
+@cache
+def blink(val, num = 1):
+    if num == 0: return [val]
+
+    if val == 0:
+        return blink(1, num-1)
+    elif len(str(val)) % 2 == 0:
+        return blink(int(str(val)[:len(str(val))//2]), num - 1) + blink(int(str(val)[len(str(val))//2:]), num - 1)
+    else:
+        return blink(val * 2024, num - 1)
+
+def solve(data):
+    print(sum([len(blink(i, 25)) for i in data]))
 
 def main():
     data = list(map(int, open(0).read().strip().split(" ")))
-    print(part1(data))
+    print(solve(data))
 
 if __name__ == "__main__":
     main()
